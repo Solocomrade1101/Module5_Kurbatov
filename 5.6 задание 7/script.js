7528064152b9cf0ad06d2a0f37b9894d418b5c1e
Solocomrade1101 <121885972+Solocomrade1101@users.noreply.github.com>
2023-02-05 11:54:00 +0300

    поправил ошибки синтаксиса

--- 5.6 задание 7/script.js
+++ 5.6 задание 7/script.js
@@ -3,13 +3,13 @@
 let arr = [1, "54", 2, "fgddf", 3, 0, 4, 5, '', 6, null, 7, 8, NaN,];
 let arrEven = [];
 let arrOdd = [];
-for (i = 0; i < arr.length; i++) {
+for (let i = 0; i < arr.length; i++) {
    if (arr[i] % 2 === 0 && typeof (arr[i]) === 'number' && arr[i] !== 0) {
       arrEven.push(arr[i])
    } else if (arr[i] % 2 !== 0 && typeof (arr[i]) === 'number' && isNaN(arr[i]) !== true) {
       arrOdd.push(arr[i])
    } else if (arr[i] == 0) {
-      console.log(arr[i]);
+      console.log(`нулевой элемент: ${arr[i]}`);
    }
 }
 console.log(arrEven.length);
